2af584b4751639204ab47ab296ea479e55219fab
laozou <xyzjhe@163.com>
2023-08-13 11:18:02 +0000

    更新 'py/py_bilibili3.py'

--- py/py_bilibili3.py
+++ py/py_bilibili3.py
@@ -150,8 +150,9 @@ class Spider(Spider):  # 元类 默认的元类 type
 
     def getCookie(self):
 
-        #在下方cookies_str  后面 双引号里面放置你的cookies
-        cookies_str = "buvid3=8B57D3BA-607A-1E85-018A-E8C430023CED42659infoc; b_lsid=BEB8EE7F_18742FF8C2E; bsource=search_baidu; _uuid=DE810E367-B52C-AF6E-A612-EDF4C31567F358591infoc; b_nut=100; buvid_fp=711a632b5c876fa8bbcf668c1efba551; SESSDATA=7624af93%2C1696008331%2C862c8%2A42; bili_jct=141a474ef3ce8cf2fedf384e68f6625d; DedeUserID=3493271303096985; DedeUserID__ckMd5=212a836c164605b7; sid=5h4ruv6o; buvid4=978E9208-13DA-F87A-3DC0-0B8EDF46E80434329-123040301-dWliG5BMrUb70r3g583u7w%3D%3D"
+       #放置你的cookies
+        rsp=self.fetch("http://127.0.0.1:9978/file/tvboxzz/cookie.txt")
+        cookies_str = rsp.text
         if cookies_str:
             cookies =  dict([co.strip().split('=') for co in cookies_str.split(';')])
             bili_jct = cookies['bili_jct']
