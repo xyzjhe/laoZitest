3f8a35c7f0f3ae96c8c91765da1f95754bf75792
laozou <xyzjhe@163.com>
2023-08-13 13:00:13 +0000

    更新 'py/py_bilibili3.py'

--- py/py_bilibili3.py
+++ py/py_bilibili3.py
@@ -157,7 +157,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         #cookies_str = rsp.text
 
         #在下方cookies_str  后面 双引号里面放置你的cookies
-        cookies_str = "buvid3=8B57D3BA-607A-1E85-018A-E8C430023CED42659infoc; b_lsid=BEB8EE7F_18742FF8C2E; bsource=search_baidu; _uuid=DE810E367-B52C-AF6E-A612-EDF4C31567F358591infoc; b_nut=100; buvid_fp=711a632b5c876fa8bbcf668c1efba551; SESSDATA=7624af93%2C1696008331%2C862c8%2A42; bili_jct=141a474ef3ce8cf2fedf384e68f6625d; DedeUserID=3493271303096985; DedeUserID__ckMd5=212a836c164605b7; sid=5h4ruv6o; buvid4=978E9208-13DA-F87A-3DC0-0B8EDF46E80434329-123040301-dWliG5BMrUb70r3g583u7w%3D%3D"
+        cookies_str = "buvid3=8905B4AA-C4CE-4976-853F-57A11EEA856018556infoc; LIVE_BUVID=AUTO8016374005231370; buvid_fp_plain=undefined; fingerprint3=d7577ba1214002a3b3e99ef5a54c11cd; b_nut=100; _uuid=2BD33E68-A3C6-35104-F58A-4491764A7ED772232infoc; buvid4=A698BE3F-38C2-EAC2-179C-A1E59A412ADB47072-022012415-5ZCNRwNsIx2%2Biu7YVq0ofQ%3D%3D; rpdid=|(u))ul)|YJ~0J'uY~|~RYu|~; fingerprint=c3e80ad851e53d02c0d45180af39c827; hit-new-style-dyn=1; CURRENT_PID=b7f74190-d1f9-11ed-b9f0-fd43ad325ff3; hit-dyn-v2=1; PVID=1; CURRENT_FNVAL=4048; nostalgia_conf=-1; b_lsid=3CB5B7CE_189CEB1B9DD; buvid_fp=c3e80ad851e53d02c0d45180af39c827; SESSDATA=15968525%2C1706942467%2C08f80%2A82xCOy0pIWE-gz6l8z4d13IvgL05RVoYl2NL_zyn4dEGN21H2VkFImw2KcN09c9U5Ena1SWQAAJgA; bili_jct=f953833b82ac300568ea66e2c3464541; DedeUserID=671023938; DedeUserID__ckMd5=48c152aca085bcce; sid=6cfd0f9o; bp_video_offset_671023938=827004396774621204"
 
         if cookies_str:
             cookies =  dict([co.strip().split('=') for co in cookies_str.split(';')])
